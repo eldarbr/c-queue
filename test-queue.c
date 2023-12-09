96ce729c15b3c4fee2e96e7162a4a77552952706
Eldar <eldar@bagirov.ru>
2023-12-09 22:29:37 +0300

    feat: add simple use example for the queue

---
+++ test-queue.c
@@ -0,0 +1,31 @@
+#include "Queue/Queue.h"
+#include <stdio.h>
+
+
+int main() {
+
+
+    QueueElement *queue;
+
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+
+    QueuePush(&queue, 10);
+
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+
+
+    QueuePush(&queue, 10);
+    QueuePush(&queue, 11);
+    QueuePush(&queue, 12);
+    QueuePush(&queue, 13);
+
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+    printf("%d %d\n", QueueGetIsLast(&queue), QueuePop(&queue));
+
+
+
+    return 0;
+}
